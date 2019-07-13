08fa21152b72549b2f5bf04d12ba08e02f837034
Q. Price <priceqp@gmail.com>
2019-07-13 14:03:59 -0500

    Add Link to Palette and passdown history through routeProps

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -34,6 +34,9 @@ const styles = {
 };
 
 class PaletteList extends React.Component {
+  goToPalette(id) {
+    this.props.history.push(`/palette/${id}`);
+  }
   render() {
     const {palettes, classes} = this.props;
     return (
@@ -44,7 +47,10 @@ class PaletteList extends React.Component {
           </nav>
           <div className={classes.palettes}>
             {palettes.map(palette => (
-              <MiniPalette {...palette} />
+              <MiniPalette 
+                {...palette} 
+                handleClick={() => this.goToPalette(palette.id)} 
+              />
             ))}
           </div>
         </div>
