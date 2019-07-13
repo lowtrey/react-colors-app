b330cbb429cc9a2eb24e8bd637df5221cf629023
Q. Price <priceqp@gmail.com>
2019-07-12 22:12:38 -0500

    Add withStyles with demo styles

---
+++ src/MiniPalette.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { withStyles } from '@material-ui/styles';
+
+const styles = {
+  main: {
+    backgroundColor: 'purple',
+    border: '3px solid teal'
+  },
+  secondary: {
+    backgroundColor: 'pink'
+  }
+};
+
+function MiniPalette(props) {
+  const {classes} = props;
+  console.log(classes);
+  
+  return (
+    <div className={classes.main}>
+      <h1>Mini Palette</h1>
+    </div>
+  );
+}
+
+export default withStyles(styles)(MiniPalette);
