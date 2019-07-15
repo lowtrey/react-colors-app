01ca78ae53531a9519a45bbd14fac8f8bef13577
Q. Price <priceqp@gmail.com>
2019-07-15 18:25:01 -0500

    Add ChromePicker and Buttons to Drawer

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import clsx from 'clsx';
 import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
@@ -10,8 +11,9 @@ import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import { ChromePicker } from 'react-color';
 
-const drawerWidth = 240;
+const drawerWidth = 400;
 
 const styles = theme => ({
   root: {
@@ -122,6 +124,16 @@ class NewPaletteForm extends React.Component {
             </IconButton>
           </div>
           <Divider />
+          <Typography variant='h4'>Design Your Palette</Typography>
+          <div>
+            <Button variant='contained' color='secondary'>Clear Palette</Button>
+            <Button variant='contained' color='primary'>Random Color</Button>
+          </div>
+          <ChromePicker 
+            color="blue" 
+            onChangeComplete={(newColor)=> console.log(newColor)} 
+          />
+          <Button variant='contained' color='primary'>Add Color</Button>
         </Drawer>
         <main
           className={clsx(classes.content, {
