29b1c0989f1cc805ce0958d74149ce759c109e2d
Q. Price <priceqp@gmail.com>
2019-07-19 02:12:08 -0500

    Prevent duplicate random colors

--- src/App.js
+++ src/App.js
@@ -98,6 +98,17 @@ class App extends React.Component {
                     </Page>
                   )} 
                 />
+                <Route 
+                  render={(routeProps) => (
+                    <Page>
+                      <PaletteList 
+                        palettes={this.state.palettes} 
+                        deletePalette={this.deletePalette}
+                        {...routeProps} 
+                      />
+                    </Page>
+                  )} 
+                />
               </Switch>
             </CSSTransition>
           </TransitionGroup>
