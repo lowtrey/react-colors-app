6c90d1b571d3add7947ee0044c5a6c37d3cd69f7
Q. Price <priceqp@gmail.com>
2019-08-20 19:55:44 -0500

    Fix eslint warning, make changes to README

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -76,6 +76,7 @@ class NewPaletteForm extends React.Component {
     while(isDuplicateColor) {
       rand = Math.floor(Math.random() * allColors.length);
       randomColor = allColors[rand];
+      // eslint-disable-next-line
       isDuplicateColor = this.state.colors.some(color => color.name === randomColor.name);
     };
     this.setState({ colors: [...this.state.colors, randomColor] });
